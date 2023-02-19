df32db83d2c94b6907cb931d706fdb678919bffa
shechin <shechinmihail@mail.ru>
2023-02-19 20:25:49 +0500

    Выполнил ДЗ 4.4

--- src/main/java/ru/hogwarts/school/controller/InfoController.java
+++ src/main/java/ru/hogwarts/school/controller/InfoController.java
@@ -6,37 +6,38 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.hogwarts.school.service.InfoService;
-
-import java.util.stream.IntStream;
+import ru.hogwarts.school.service.InfoServiceImpl;
 
 @RestController
 public class InfoController {
 
-    private final InfoService infoService;
+    private final InfoServiceImpl infoService;
 
     private static final Logger logger = LoggerFactory.getLogger(InfoService.class);
 
-    public InfoController(InfoService infoService) {
+    public InfoController(InfoServiceImpl infoService) {
         this.infoService = infoService;
     }
 
-    @GetMapping(path = "/getPort")   //GET http://localhost:8080/getPort
+    @GetMapping(path = "/getPort")   //GET http://localhost:8080/info/getPort
     public ResponseEntity<Integer> getPortInfo() {
+        logger.info("Вызван метод порта");
         return ResponseEntity.ok(infoService.portNumber());
     }
 
-    @GetMapping(path = "/getValue")   //GET http://localhost:8080/getValue
-    public ResponseEntity<Integer> getValue() {
-        long startTime = System.currentTimeMillis();
-
-        int sum = IntStream.iterate(1, a -> a + 1)
-                .limit(1_000_000)
-                .parallel()
-                .reduce(0, Integer::sum);
-
-        long endTime = System.currentTimeMillis();
-        logger.warn("Метод работал " + (endTime - startTime) + "ms");
-
-        return ResponseEntity.ok(sum);
-    }
+//
+//    @GetMapping(path = "/getValue")   //GET http://localhost:8080/getValue
+//    public ResponseEntity<Integer> getValue() {
+//        long startTime = System.currentTimeMillis();
+//
+//        int sum = IntStream.iterate(1, a -> a + 1)
+//                .limit(1_000_000)
+//                .parallel()
+//                .reduce(0, Integer::sum);
+//
+//        long endTime = System.currentTimeMillis();
+//        logger.warn("Метод работал " + (endTime - startTime) + "ms");
+//
+//        return ResponseEntity.ok(sum);
+//    }
 }
--- src/main/java/ru/hogwarts/school/service/InfoServiceImpl.java
+++ src/main/java/ru/hogwarts/school/service/InfoServiceImpl.java
@@ -1,14 +1,12 @@
 package ru.hogwarts.school.service;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
-@Profile("production")
 public class InfoServiceImpl implements InfoService {
 
-    @Value("${server.port}")
+    @Value("8080")
     private Integer serverPort;
 
     @Override
